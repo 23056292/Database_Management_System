a87c4cede88d073605e06d51ad9b3b11bf28bfb7
Prabhat Lamichhane <prabhatlc07@gmail.com>
2026-01-21 14:01:48 +0545

    This is commit for auth part complete with backend

--- Documents/JournalApp-main/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/JournalApp.AssemblyInfo.cs
+++ Documents/JournalApp-main/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/JournalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+56c92423286eebf5be51c561f6ecd8ac30f364a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
